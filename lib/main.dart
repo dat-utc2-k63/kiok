477524100ee744483b304775fcd7e05f213ecce5
Thanh Dat <thanhdat4104@gmail.com>
2025-04-01 14:15:14 +0700

    connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'screen/main_menu_screen.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(SchoolServiceApp());
 }
 
